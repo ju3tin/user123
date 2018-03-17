6b8a44ced6f748b6696043b4946a777b91a3f818
justin <justin@justins-MacBook-Pro.local>
2018-03-17 14:05:00 +0000

    asd

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head><script>
+window.onload = function() {
+    // similar behavior as an HTTP redirect
+    window.location.replace("http://visulizingexcludtion.mybluemix.net/demos/");
+}
+
+</script></head>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+</body>
+</html>
